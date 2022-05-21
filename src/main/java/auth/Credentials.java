8f1493524a14902ced66177ef7b95ee11486a15c
Gabriel Castro <gabrielcastrog2@gmail.com>
2022-05-21 12:45:58 -0300

    add: Some config at mqtt client

--- src/main/java/auth/Credentials.java
+++ src/main/java/auth/Credentials.java
@@ -1,4 +1,38 @@
 package auth;
 
 public class Credentials {
+
+    private static String host = "f5c333233b4844a3a3c621706d87d512.s1.eu.hivemq.cloud";
+    private static String userName = "teste_origem_motos";
+    private static String password = "Origemmotos8";
+
+    public Credentials(String host, String userName, String password) {
+        this.host = host;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public static String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public static String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/mqttclient/Client.java
+++
@@ -1,4 +0,0 @@
-package mqttclient;
-
-public class Client {
-}
